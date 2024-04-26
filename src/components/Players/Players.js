06272ed45252232c8f33bcbdc72e9c18ea7a824a
kulikq <artemkulik2004@mail.ru>
2024-04-26 15:09:29 +0300

    edit navigation

--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './Players.css';
 import Boosts from '../Boosts/Boosts';
 import Trade from '../Trade/Trade';
-import Navigation from '../Navigation/Navigation'; // Если у вас есть общий компонент навигации
 
 const Players = () => {
     const [currentPage, setCurrentPage] = useState('players');
@@ -57,6 +56,17 @@ const Players = () => {
                 return null;
         }
     };
+    return (
+        <div className="players-container">
+            <div className="navigation">
+                <button onClick={() => setCurrentPage('players')}>Players</button>
+                <button onClick={() => setCurrentPage('boosts')}>Boosts</button>
+                <button onClick={() => setCurrentPage('trade')}>Trade</button>
+            </div>
+            {renderContent()}
+        </div>
+    );
+
 };
 
 export default Players;
