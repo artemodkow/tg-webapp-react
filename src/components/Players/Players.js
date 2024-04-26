6a4bf9effca2f172958e559b087b937e1afe0055
kulikq <artemkulik2004@mail.ru>
2024-04-26 14:56:32 +0300

    configurate menu players

--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -2,14 +2,53 @@ import React, { useState } from 'react';
 import './Players.css';
 import Boosts from '../Boosts/Boosts';
 import Trade from '../Trade/Trade';
+import Navigation from '../Navigation/Navigation'; // Если у вас есть общий компонент навигации
 
 const Players = () => {
     const [currentPage, setCurrentPage] = useState('players');
+    // Предположим, что данные о работниках и пользователе загружаются откуда-то
+    const userData = {
+        avatar: "path_to_avatar",
+        name: "Thomas Vien",
+        coins: 56500,
+        rating: 10220,
+        // другие данные пользователя...
+    };
+    const employees = [
+        // Загруженные данные о работниках
+    ];
 
     const renderContent = () => {
         switch (currentPage) {
             case 'players':
-                return <div>Контент страницы Players</div>;
+                // Возвращаем JSX для списка работников, который должен быть загружен
+                return (
+                    <>
+                        <div className="user-info">
+                            <img src={userData.avatar} alt="User Avatar" className="user-avatar"/>
+                            <div className="user-name">{userData.name}</div>
+                            <div className="user-rating">
+                                <img src="path_to_rating_icon" alt="Rating Icon" className="rating-icon"/>
+                                <div>{userData.rating}</div>
+                            </div>
+                            <div className="big-balance">
+                                <img src="path_to_coin_icon" alt="Coin Icon" className="coin-icon"/>
+                                {userData.coins}
+                            </div>
+                            <div className="referral-link">Если другие игроки перейдут по вашей ссылке...</div>
+                        </div>
+                        <div className="employees-container">
+                            <div className="employees-header">Мои работники: {employees.length}</div>
+                            {employees.map(employee => (
+                                <div key={employee.id} className="employee-item">
+                                    <img src={employee.avatar} alt={employee.name} className="employee-avatar"/>
+                                    <div className="employee-name">{employee.name}</div>
+                                    <div>{employee.income} /min</div>
+                                </div>
+                            ))}
+                        </div>
+                    </>
+                );
             case 'boosts':
                 return <Boosts />;
             case 'trade':
@@ -21,12 +60,8 @@ const Players = () => {
 
     return (
         <div className="players-container">
-            <div className="navigation">
-                <button onClick={() => setCurrentPage('players')}>Players</button>
-                <button onClick={() => setCurrentPage('boosts')}>Boosts</button>
-                <button onClick={() => setCurrentPage('trade')}>Trade</button>
-            </div>
             {renderContent()}
+            <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
         </div>
     );
 };
