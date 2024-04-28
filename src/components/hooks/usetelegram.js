f3f7073eb9182205638a78ad6e29e30df349cbc1
kulikq <artemkulik2004@mail.ru>
2024-04-28 18:23:47 +0300

    error with apple 4

--- src/components/hooks/usetelegram.js
+++ src/components/hooks/usetelegram.js
@@ -1,3 +1,4 @@
+/*
 const tg = window.Telegram.WebApp;
 
 export  function usetelegram() {
@@ -5,6 +6,5 @@ export  function usetelegram() {
     return{
         tg,
         user: tg.initDataUnsafe?.user,
-        avatar: tg.initData.user?.photo_url,
     }
-}
+}*/
