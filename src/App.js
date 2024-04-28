8a72402d493d7c56828a7678f859fed0bb2ce1b9
kulikq <artemkulik2004@mail.ru>
2024-04-28 19:00:48 +0300

    error apple 6

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@
 import React, {useEffect} from 'react';
 import Players from './components/Players/Players';
 import './App.css';
-import {usetelegram} from "./components/hooks/usetelegram";
 
 
 const App = () => {
-    const tg = window.Telegram.WebApp;
     return (
         <div className="App">
             <Players />
