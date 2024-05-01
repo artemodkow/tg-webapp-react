3c50485ff32ec4ad98be9d83e5e89553a9f56273
kulikq <artemkulik2004@mail.ru>
2024-05-02 02:02:25 +0300

    players

--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -3,30 +3,45 @@ import './Players.css';
 
 const players = [
     // Эти данные должны быть получены из API
-    { id: 1, name: 'Ева Ефимова', price: 5000, avatar: 'path_to_avatar', status: 'Работает на Fierro' },
-    { id: 2, name: 'Петя Клюкин', price: 1000, avatar: 'path_to_avatar', status: 'Работает на Nikita' },
+    { id: 1, name: 'Ева Ефимова', price: 5000, avatar: process.env.PUBLIC_URL + "/icons/avatar.png", status: 'Работает на Fierro' },
+    { id: 2, name: 'Петя Клюкин', price: 1000, avatar: process.env.PUBLIC_URL + "/icons/avatar.png", status: 'Работает на Nikita' },
     // ... другие игроки
 ];
 
 const Trade = () => {
     return (
         <div className="trade-container">
+
             <h1 className="trade-header">Покупка игроков</h1>
+
             <ul className="player-list">
                 {players.map(player => (
                     <li key={player.id} className="player-item">
-                        <img src={player.avatar} alt={player.name} className="player-avatar" />
+
+                        <img src={player.avatar} alt={player.name} className="player-avatar"/>
+
                         <div className="player-info">
-                            <h2 className="player-name">{player.name}</h2>
-                            <p className="player-status">{player.status}</p>
+
+                            <div className="player-name">{player.name}</div>
+                            <div className="player-status">{player.status}</div>
+
                         </div>
+
                         <div className="player-price">
-                            <span>{player.price}</span> 🪙
+                            <span>
+                                {player.price}
+                            </span>
+                            <span><img src={process.env.PUBLIC_URL + "/icons/Монетка золотая право.png"} alt="Coin Icon"
+                                       className="coin-price"/>
+                            </span>
+                        </div>
+                        <div>
+                            <img src={process.env.PUBLIC_URL + "/icons/go.png"} alt="go" className="go"/>
                         </div>
                     </li>
                 ))}
             </ul>
-            {/* Предполагается, что навигация уже существует и её стили определены */}
+
         </div>
     );
 };
