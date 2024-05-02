304d0cf2b60943106fb24d419b9f4421774b814d
kulikq <artemkulik2004@mail.ru>
2024-05-02 13:26:54 +0300

    trade

--- src/components/Trade/Trade.js
+++ src/components/Trade/Trade.js
@@ -19,22 +19,36 @@ const Trade = () => {
             <div className="balance-amount">
                 4,335
                 <span>
-                    <img src={process.env.PUBLIC_URL + "/icons/star.png"} alt="Star" className="Star" />
+                    <img src={process.env.PUBLIC_URL + "/icons/star.png"} alt="Star" className="Star"/>
                 </span>
             </div>
             <div className="header-stats">
-                Статистика
+                Статистика:
             </div>
             <div className="members-list">
                 {members.map(member => (
+
                     <div key={member.level} className="member-item">
                         {member.level}: {member.membersCount}
                     </div>
+
                 ))}
             </div>
             <button className="trade-button">
                 Trade
             </button>
+            <div>
+                <button className="rules-button">
+                    <a
+                        href="https://t.me/share/url?url=https://t.me/sharecointestbot&text"
+                        target="_blank"
+                        className="rules-link"
+                    >
+                        Правила пользования
+                    </a>
+                </button>
+            </div>
+
         </div>
     );
 };
