0e126c9f4db9f43b4e7c03845d15e518dcf61955
kulikq <artemkulik2004@mail.ru>
2024-05-03 21:09:55 +0300

    player in menu

--- src/components/EmployeeDetails/EmployeeDetails.js
+++ src/components/EmployeeDetails/EmployeeDetails.js
@@ -10,24 +10,59 @@ const EmployeeDetails = ({employee, onUpgrade, onProtect }) => {
             <div className="employee-header">
                 <div className="employee-info">
                     <h1>{employee.name}</h1>
-                    <p>Работает на вас</p>
+                    <p className="working">Работает на вас</p>
                 </div>
             </div>
 
             <div>
-                <p>{employee.protected ? `Защищен на ${employee.protectionHours} часов` : 'Не защищен'}</p>
+                <span
+                    className="hours-protect">{employee.protected ? `Защищен на ${employee.protectionHours} часов` : 'Не защищен'}</span>
+                <span>{employee.rate} /min</span>
             </div>
 
             <div>
-                <p>{employee.rate} ₽/min</p>
+
             </div>
 
             <div className="line">
                 <img src={process.env.PUBLIC_URL + "/icons/line.png"} alt="line"/>
             </div>
             <div className="employee-actions">
-                <button onClick={() => onUpgrade(employee.id)}>Прокачать за N ₽</button>
-                <button onClick={() => onProtect(employee.id)}>Защитить за N ₽ на 8 часов</button>
+                <button onClick={() => onUpgrade(employee.id)}>Прокачать за N
+                    <img src={`${process.env.PUBLIC_URL}/icons/Монетка золотая право.png`} alt="Coins"
+                         className="coin-icon"/>
+                </button>
+                <button onClick={() => onProtect(employee.id)}>Защитить за N
+                    <img src={`${process.env.PUBLIC_URL}/icons/Монетка золотая право.png`} alt="Coins"
+                         className="coin-icon"/>
+                    на 8 часов
+                </button>
+            </div>
+
+            <div className="referal-friend">
+                <div className="level">
+                    C:
+                </div>
+            </div>
+            <div className="referal-friend">
+                <div className="level">
+                    D:
+                </div>
+            </div>
+            <div className="referal-friend">
+                <div className="level">
+                    E:
+                </div>
+            </div>
+            <div className="referal-friend">
+                <div className="level">
+                    F:
+                </div>
+            </div>
+            <div className="referal-friend">
+                <div className="level">
+                    G:
+                </div>
             </div>
         </div>
     );
